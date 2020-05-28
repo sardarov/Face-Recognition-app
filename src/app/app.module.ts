0e638d59ac522bca96c06662b08893680f7f10e9
sardarov <sardarovsardar@gmail.com>
2020-05-28 02:40:21 +0200

    adding nav-menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ToasterModule } from 'angular2-toaster';
 import { FormsModule } from '@angular/forms';
+
+import {FaceRecognitionService } from './services/face-recognition.service'
+
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { FaceAnalyzeComponent } from './face-analyze/face-analyze.component';
@@ -17,9 +22,11 @@ import { FaceAnalyzeComponent } from './face-analyze/face-analyze.component';
     FormsModule,
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule,
+    ToasterModule
   ],
-  providers: [],
+  providers: [FaceRecognitionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
